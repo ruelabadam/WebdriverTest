200209eeb1283534a434066b02cbbf8db22a0597
Ruel Abadam <ruelabadam@10.0.1.45>
2017-04-25 13:58:58 -0700

    Test sauce!

--- src/test/example/NewTest.java
+++ src/test/example/NewTest.java
@@ -7,13 +7,25 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class NewTest {
+	
+//	public static final String USERNAME = sUSERNAME;
+//	public static final String ACCESS_KEY = sACCESS_KEY;
+	public static final String URL = "https://" + sUSERNAME + ":" + sACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+	
+	private WebDriver driver;
 
-	private WebDriver driver; 
 	@BeforeTest
-	public void beforeTest(){
-	 driver = new ChromeDriver();
+	public void beforeTest() throws MalformedURLException {
+		  DesiredCapabilities caps = DesiredCapabilities.chrome();
+		  driver = new RemoteWebDriver(new URL(URL), caps);
+//	 driver = new ChromeDriver();
 	}
 		
 	@AfterTest

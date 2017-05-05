3feeb817d438b305c00dd8db6f96f4fd63af99ea
Ruel Abadam <ruelabadam@10.0.1.45>
2017-05-04 22:15:56 -0700

    RemoteTest

--- src/test/example/RemoteTest.java
+++ src/test/example/RemoteTest.java
@@ -10,44 +10,31 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
 import java.net.URL;
 
-public class NewTest {
+public class RemoteTest {
 	
-	
-//	public final String user = System.getProperty("sUSERNAME") ;
-//	public final String key = System.getProperty("sACCESS_KEY") ;
 	
 	public final String scURL = "https://" + System.getProperty("sUSERNAME") + ":" + System.getProperty("sACCESS_KEY") + "@ondemand.saucelabs.com:443/wd/hub";
 	
 	public RemoteWebDriver driver;
-
 	
-	public void setup() throws Throwable {
-		
-		//codeship Chrome
-//		System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
-		
-		// Initialize browser
-//		driver=new ChromeDriver();
-		
-		@SuppressWarnings("static-access")
-		DesiredCapabilities caps = new DesiredCapabilities().chrome();
-  		  caps.setCapability("platform", "Linux");
-  		  caps.setCapability("version", "47.0");
-  		 driver = new RemoteWebDriver(new URL(scURL), caps);
-  		 
-  		 System.out.println("connection should be done.");
+	@BeforeTest
+	public void beforeTest() throws Throwable{
+	new DesiredCapabilities();
+	DesiredCapabilities caps = DesiredCapabilities.chrome();
+	  caps.setCapability("platform", "Linux");
+	  caps.setCapability("version", "56.0");
+	driver = new RemoteWebDriver(new URL(scURL), caps);
 	}
-		
-//	@AfterMethod
-//	public void afterTest() {
-//			driver.quit();			
-//	}	
+	
+
 	
 	@Test
 	public void testEasy() throws InterruptedException {
-		driver.get("http://www.Ruelsrunning.com");  
+		driver.get("http://www.ruelsrunning.com");  
 		Thread.sleep(5000);
 		String title = driver.getTitle();	
 		Thread.sleep(5000);

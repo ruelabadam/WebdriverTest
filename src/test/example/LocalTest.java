4e979841fea64790495593db3579bf76a40029ae
Ruel Abadam <ruelabadam@10.0.1.45>
2017-05-04 21:55:42 -0700

    Local test

---
+++ src/test/example/LocalTest.java
@@ -0,0 +1,32 @@
+package example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.*;
+import org.testng.Assert;		
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class LocalTest {
+
+	private WebDriver driver; 
+	@BeforeTest
+	public void beforeTest(){
+	 driver = new ChromeDriver();
+	}
+		
+	@AfterTest
+	public void afterTest() {
+			driver.quit();			
+	}	
+	
+	@Test
+	public void testEasy() {
+		driver.get("http://www.ruelsrunning.com");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Ru El's Running - In here...everyone's a runner!"));
+	}
+	
+
+}
